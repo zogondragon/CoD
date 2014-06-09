06143075e22b3586b12270bcecd0b5564ca54aa8
Gunhee Lee <teuksu00@gmail.com>
2014-06-10 01:12:54 +0900

    ServerInterface added.

---
+++ CoDServer/ServerInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoDServer
+{
+    // It controls information flow between (CoDServer) and (CoDClient).
+    // It is abstract class because the interface can be anything including TCP, UDP, Pipe and Txt.
+    abstract class ServerInterface
+    {
+        public virtual string ReadLine();
+        public virtual void WriteLine();
+    }
+
+    // For example, it gets input from localhost:63999
+    public class TCPServerInterface : ServerInterface
+    {
+        // TODO: implement this
+    }
+
+    // Gets command from console, return the result to stdout
+    public class ConsoleServerInterface : ServerInterface
+    {
+        public override string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public override void WriteLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
